d1a0de0aa909385f3c90e5d7b8c7e8489fd265f0
unknown <ahedqaood19@gmail.com>
2019-10-02 00:32:54 +0300

    include functions definitions

---
+++ initializations.h
@@ -0,0 +1,11 @@
+#ifndef initializations_h
+#define initializations_h
+
+void defineTrafficLight1();
+void defineTrafficLight2();
+void defineTrafficLight3();
+void defineTrafficLight4();
+
+void defineReceiver();
+
+#endif
