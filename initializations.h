e1a4aa5c5c9391da972f9980405f3d4bec0d1a73
unknown <ahedqaood19@gmail.com>
2019-10-02 00:58:02 +0300

    add variables

--- initializations.h
+++ initializations.h
@@ -1,6 +1,31 @@
 #ifndef initializations_h
 #define initializations_h
 
+/*------ Variables Initialization ------*/
+// variables for traffic light number 1
+unsigned char green1  = 1;
+unsigned char orange1 = 2;
+unsigned char red1    = 3;
+
+// variables for traffic light number 2
+unsigned char green2  = 4;
+unsigned char orange2 = 5;
+unsigned char red2    = 6;
+
+// variables for traffic light number 3
+unsigned char green3  = 7;
+unsigned char orange3 = 8;
+unsigned char red3    = 9;
+
+// variables for traffic light number 4
+unsigned char green4  = 10;
+unsigned char orange4 = 11;
+unsigned char red4    = 12;
+
+// IR receiver
+unsigned char irReciver = 13;
+
+/*------ Define Functions ------*/
 void defineTrafficLight1();
 void defineTrafficLight2();
 void defineTrafficLight3();
