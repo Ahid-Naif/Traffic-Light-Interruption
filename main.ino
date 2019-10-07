fe0cb4abf3e8b5c6a8dca68c7f8f436dd689de49
unknown <ahedqaood19@gmail.com>
2019-10-08 00:24:30 +0300

    add traffic light LOGIC

--- main.ino
+++ main.ino
@@ -1,5 +1,11 @@
 #include "initializations.h"
-#include "sequence.h"
+#include "sequences.h"
+
+unsigned long onTime   = 5000; // ON   Time of 5000 ms (5 seconds)
+unsigned long waitTime = 2000; // Wait Time of 2000 ms (2 seconds)
+unsigned long previousTime, currentTime;
+
+int sequenceCounter = 0;
 
 void setup(){
     // define traffic light LEDs as INPUT
@@ -12,8 +18,70 @@ void setup(){
     defineReceiver();
 
     // set all traffic lights in initial state
-    sequence0();
-
+    sequence0(); // to ensure all LEDs are OFF
+    sequence1();
+    sequenceCounter++;
+    previousTime = millis(); // initializa previousTime variable
+    for(;;){
+        currentTime = millis();
+        if(sequenceCounter == 1){
+            if(currentTime - previousTime >= onTime){
+                sequence2();
+                sequenceCounter++;  
+                previousTime = currentTime;
+            }
+        }
+        else if(sequenceCounter == 2){
+            if(currentTime - previousTime >= waitTime){
+                sequence3();
+                sequenceCounter++;
+                previousTime = currentTime;
+            }
+        }
+        else if(sequenceCounter == 3){
+            if(currentTime - previousTime >= onTime){
+                sequence4();
+                sequenceCounter++;
+                previousTime = currentTime;
+            }
+        }
+        else if(sequenceCounter == 4){
+            if(currentTime - previousTime >= waitTime){
+                sequence5();
+                sequenceCounter++;
+                previousTime = currentTime;
+            }
+        }
+        else if(sequenceCounter == 5){
+            if(currentTime - previousTime >= onTime){
+                sequence6();
+                sequenceCounter++;
+                previousTime = currentTime;
+            }
+        }
+        else if(sequenceCounter == 6){
+            if(currentTime - previousTime >= waitTime){
+                sequence7();
+                sequenceCounter++;
+                previousTime = currentTime;
+            }
+        }
+        else if(sequenceCounter == 7){
+            if(currentTime - previousTime >= onTime){
+                sequence8();
+                sequenceCounter++;
+                previousTime = currentTime;
+            }
+        }
+        else if(sequenceCounter == 8){
+            if(currentTime - previousTime >= waitTime){
+                sequence1();
+                sequenceCounter = 1;
+                previousTime = currentTime;
+            }
+        }
+        
+    }
 }
 
 void loop(){
