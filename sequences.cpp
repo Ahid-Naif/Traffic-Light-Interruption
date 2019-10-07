cb756f46374be8478457500c27dda04ab7383de3
unknown <ahedqaood19@gmail.com>
2019-10-08 00:24:08 +0300

    add traffic light sequences

---
+++ sequences.cpp
@@ -0,0 +1,64 @@
+#include "sequences.h"
+
+void sequence0(){
+    initialize_traffic1();
+    initialize_traffic2();
+    initialize_traffic3();
+    initialize_traffic4();
+}
+
+void sequence1(){
+    traffic1_go();
+    traffic2_stop();
+    traffic3_stop();
+    traffic4_stop();
+}
+
+void sequence2(){
+    traffic1_wait();
+    traffic2_stop();
+    traffic3_stop();
+    traffic4_stop();
+}
+
+void sequence3(){
+    traffic1_stop();
+    traffic2_go();
+    traffic3_stop();
+    traffic4_stop();
+}
+
+void sequence4(){
+    traffic1_stop();
+    traffic2_wait();
+    traffic3_stop();
+    traffic4_stop();
+}
+
+void sequence5(){
+    traffic1_stop();
+    traffic2_stop();
+    traffic3_go();
+    traffic4_stop();
+}
+
+void sequence6(){
+    traffic1_stop();
+    traffic2_stop();
+    traffic3_wait();
+    traffic4_stop();
+}
+
+void sequence7(){
+    traffic1_stop();
+    traffic2_stop();
+    traffic3_stop();
+    traffic4_go();
+}
+
+void sequence8(){
+    traffic1_stop();
+    traffic2_stop();
+    traffic3_stop();
+    traffic4_wait();
+}
---
+++ sequences.h
@@ -0,0 +1,16 @@
+#ifndef sequences_h
+#define sequences_h
+
+#include "traffic_light_modes.h"
+
+void sequence0();
+void sequence1();
+void sequence2();
+void sequence3();
+void sequence4();
+void sequence5();
+void sequence6();
+void sequence7();
+void sequence8();
+
+#endif
